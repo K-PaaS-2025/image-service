47f1bcfa4d28663e946ce2833f7418e4f47a9446
후니 <hipark@huniui-MacBookPro.local>
2025-10-05 14:57:24 +0900

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title='K-PaaS 2025 Image Service',
+    summary='Image Service for K-PaaS 2025',
+    docs_url='/',
+)
+
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
